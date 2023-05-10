d593cd208ad84a432d5093d4e9cb37a352a2be43
Martinsr99 <silesreche.martin@gmail.com>
2023-05-10 19:36:59 +0200

    feat. Add CRUD for users

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,26 +1,96 @@
-const usuario = require("../models/usuario")
+const usuario = require("../models/usuario");
+const bcrypt = require("bcryptjs");
 
+const getUsuarios = async (req, res) => {
+  const usuarios = await usuario.find();
 
-const getUsuarios = async(req,res) =>{
+  res.json({
+    ok: true,
+    usuarios,
+  });
+};
 
-    const usuarios = await usuario.find()
+const getUsuarioById = async (req, res) => {
+  const usuarioById = await usuario.findById(req.params.id);
 
-    res.json({
-        ok:true,
-        usuarios
-    })
-}
+  res.json({
+    ok: true,
+    usuarioById,
+  });
+};
 
-const crearUsuario = async(req, res) =>{
-    const {email,name,password} = req.body
+const borrarUsuario = async (req, res) => {
+  try {
+    const existingUser = await usuario.findById(req.params.id);
+    if (!existingUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    await usuario.findByIdAndDelete(req.params.id);
+  } catch (error) {
+    res.status(500).json({
+      msg: "No se pudo borrar el usuario",
+      ok: false,
+    });
+  }
 
-    const usuarioNuevo = new usuario(req.body)
+  res.json({
+    ok: true,
+    msg: "Usuario borrado",
+  });
+};
 
+const crearUsuario = async (req, res) => {
+  const { email, name, password } = req.body;
+  const existingUser = await usuario.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ error: "Email already exists" });
+  }
 
-    await usuarioNuevo.save()
+  const usuarioNuevo = new usuario(req.body);
 
-    res.json({ok:true,usuarioNuevo})
+  //Encriptar contraseña
+  const salt = bcrypt.genSaltSync();
+  usuarioNuevo.password = bcrypt.hashSync(String(password), salt);
 
-}
+  //Guardar usuario
+  await usuarioNuevo.save();
 
-module.exports = {getUsuarios,crearUsuario}
+  res.json({ ok: true, usuarioNuevo });
+};
+
+const updateUsuario = async (req, res) => {
+  const { password, google, email, ...campos } = req.body;
+
+  uid = req.params.id;
+
+  const existingUser = await usuario.findById(uid);
+  if (!existingUser) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  if (existingUser.email !== email) {
+    const existeEmail = await usuario.findOne({ email });
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Ya existe un usuario con ese correo",
+      });
+    }
+  }
+
+  campos.email = email;
+
+  const usuarioActualizado = await usuario.findByIdAndUpdate(uid, campos, {
+    new: true,
+  });
+
+  res.json({ ok: true, usuarioActualizado });
+};
+
+module.exports = {
+  getUsuarios,
+  crearUsuario,
+  getUsuarioById,
+  updateUsuario,
+  borrarUsuario,
+};
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,11 +1,38 @@
+const { Router } = require("express");
+const {
+  getUsuarios,
+  crearUsuario,
+  getUsuarioById,
+  updateUsuario,
+  borrarUsuario
+} = require("../controllers/usuarios");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
 
-const {Router} = require('express') 
-const {getUsuarios,crearUsuario} = require('../controllers/usuarios')
+const router = Router();
 
-const router = Router()
+router.get("/", getUsuarios);
+router.get("/:id", getUsuarioById);
+router.post(
+  "/",
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("password", "La contraseña es obligatoria").not().isEmpty(),
+    check("email", "El correo es obligatorio").isEmail(),
+    validarCampos,
+  ],
+  crearUsuario
+);
+router.put(
+  "/:id",
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("role", "El rol es obligatorio").not().isEmpty(),
+    check("email", "El correo es obligatorio").isEmail(),
+    validarCampos,
+  ],
+  updateUsuario
+);
 
-router.get('/', getUsuarios)
-router.post('/', crearUsuario)
-
-
-module.exports = router
+router.delete("/:id", borrarUsuario);
+module.exports = router;
